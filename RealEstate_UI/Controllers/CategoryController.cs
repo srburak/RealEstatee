355e9be09027c0ba16ddb1f0815ea542a4b6208f
Burak Kaymaz <burakkymz@gmail.com>
2024-10-14 21:20:35 +0300

    Add DTOs and update CategoryController for API integration
    Introduced new DTO classes for category data handling: CreateCategoryDto, GetByIdCategoryDto, ResultCategoryDto, and UpdateCategoryDto. Updated CategoryController to fetch and display category data asynchronously from an API. Modified Index.cshtml to render a table of categories and included necessary namespace imports.

--- RealEstate_UI/Controllers/CategoryController.cs
+++ RealEstate_UI/Controllers/CategoryController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_UI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync(
+                "https://localhost:44364/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
