c7c577061d2c1abeca78bb46ce4d0da348ef5697
Burak Kaymaz <burakkymz@gmail.com>
2024-10-20 17:58:47 +0300

    Add roles, JWT, SignalR, and new controllers
    - Added `RolesType` enum in `RealEstate_Api.Enums` with values `Admin`, `Member`, `Visitor`, `Manager`, and `Agent`.
    - Introduced `IHttpClientFactory` dependency in `SignalRHub` and added `SendCategoryCount` method to fetch and broadcast category count data.
    - Created `GetCheckAppUserViewModel` class in `RealEstate_Api.Tools` with properties `Id`, `Username`, `Role`, and `IsExist`.
    - Added `JwtTokenDefaults` class in `RealEstate_Api.Tools` for JWT token configuration constants.
    - Implemented `JwtTokenGenerator` class in `RealEstate_Api.Tools` with `GenerateToken` method to create JWT tokens.
    - Added `TokenResponseViewModel` class in `RealEstate_Api.Tools` with properties `Token` and `ExpireDate`.
    - Introduced `LoginController` in `RealEstate_UI.Controllers` with an `Index` action.
    - Added `TestController` in `RealEstate_UI.Controllers` with an `Index` action.
    - Updated `Index.cshtml` to include SignalR functionality, establishing a connection and updating the UI with category count data.

---
+++ RealEstate_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "http://localhost";
+        public const string ValidIssuer = "http://localhost";
+        public const string Key = "REalEstate..0102030405Asp.NetCore8.0.1+-*/";
+        public const int Expire = 5;
+    }
+}
