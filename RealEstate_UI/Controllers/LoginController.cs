eb36f17b1e617200bffd71e12b8ef5ec5ca6524f
Burak Kaymaz <burakkymz@gmail.com>
2024-10-23 17:37:07 +0300

    Add JWT authentication and login functionality
    Updated LoginController.cs to handle JWT tokens and user login via an external API. Introduced IHttpClientFactory dependency. Added CreateLoginDto and GetAppUserIdDto classes for capturing login credentials and user ID. Added JwtResponseModel class for handling JWT token and expiration date. Updated RealEstate_UI.csproj with necessary package references. Added Index.cshtml as the login page with form fields and styling.

--- RealEstate_UI/Controllers/LoginController.cs
+++ RealEstate_UI/Controllers/LoginController.cs
@@ -1,11 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_UI.Dtos.LoginDtos;
+using RealEstate_UI.Models;
 
 namespace RealEstate_UI.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public LoginController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+        {
+            var client = _clientFactory.CreateClient();
+            var content = new StringContent(JsonSerializer.Serialize(createLoginDto),Encoding.UTF8,"application/json");
+            var response = await client.PostAsync("https://localhost:44364/api/Login", content);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>
+                    (jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+
+                if (tokenModel != null)
+                {
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    var token = handler.ReadJwtToken(tokenModel.Token);
+                    var claims = token.Claims.ToList();
+                    if (tokenModel.Token != null)
+                    {
+                        claims.Add(new Claim("realestatetoken",tokenModel.Token));
+                        var claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                        var authProperties = new AuthenticationProperties
+                        {
+                            ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30),
+                            IsPersistent = true
+                        };
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                        return RedirectToAction("Index", "MyAdverts");
+                    }
+                }
+            }
             return View();
         }
     }
---
+++ RealEstate_UI/Dtos/LoginDtos/GetAppUserIdDto.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstate_UI.Dtos.LoginDtos
+{
+    public class GetAppUserIdDto
+    {
+        public int UserId { get; set; }
+    }
+}
